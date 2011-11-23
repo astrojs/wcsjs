d50b8b70ab506183b021aecab6c2b7dda32fdf38
Amit Kapadia <amit@zooniverse.org>
2011-11-23 13:47:06 -0600

    Created JS file and started the linear tranformations.

---
+++ wcs.js
@@ -0,0 +1,44 @@
+(function () {
+	"use strict"
+	
+	var WCS = function (header) {
+		this.naxis;
+		this.crpix;
+		this.cdelt;
+		this.pc;
+		this.cd;
+		this.cunit;
+	};
+	
+	WCS.prototype = {
+
+		pxl_to_proj_plane: function (pts) {
+
+			var ii, jj, num_points, point, proj_plane;
+
+			num_points = pts.length;
+
+			// Loop over the number of points
+			for (ii = 0; ii < num_points; ii += 1) {
+				// Transform the point
+				point = 0;
+				for (jj = 0; jj < this.naxis; jj += 1) {
+					point += this.cd[2*ii + jj] * (pts[jj] - crpix[jj]);
+				}
+				proj_plane[ii] = point;
+			}			
+			return proj_plane;
+		},
+		
+		proj_plane_to_spherical: function (x, y) {
+			
+		},
+		
+		spherical_to_celestial: function (phi, theta) {
+			
+		},
+		
+	};
+
+	self.WCS = WCS;
+}());
