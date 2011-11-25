c107269222694d21a639a1cd6e06ee5a3d9fc6c7
Amit Kapadia <amit@zooniverse.org>
2011-11-24 23:38:41 -0600

    wcs.js converts pixel to sky coordinates for TAN projections

--- blah.js
+++ blah.js
@@ -10,16 +10,9 @@ $(document).ready(function() {
 	dec = $('#dec');
 	
 	// Get the JSON header
-	$.getJSON('ngc2967-g.json', function(json) {
+	$.getJSON('example-wcs/json/1904-66_TAN.json', function(json) {
 		wcs = new WCS(json);
-		coords = wcs.pix_to_sky([300, 200]);
-		console.log(coords);
+		coords = wcs.pix_to_sky([0, 0]);		
 	});
-	
-	// x.text('x coord');
-	// y.text('y coord');
-	// ra.text('right ascension');
-	// dec.text('declination');
-	
-	
+
 });
--- wcs.js
+++ wcs.js
@@ -15,55 +15,38 @@ function rad_to_deg(radian) {
 		
 		// Parse a JSON object for WCS data
 		this.wcsaxes = parseInt(hdr.wcsaxes);
-		this.crpix = [
-			parseFloat(hdr.crpix1),
-			parseFloat(hdr.crpix2)
-		];
-		this.pc = [
-			[parseFloat(hdr.pc1_1), parseFloat(hdr.pc1_2)],
-			[parseFloat(hdr.pc2_1), parseFloat(hdr.pc2_2)]
-		];
-		this.cdelt = [
-			parseFloat(hdr.cdelt1),
-			parseFloat(hdr.cdelt2)
-		];
+		this.crpix = [parseFloat(hdr.crpix1) - 1, parseFloat(hdr.crpix2) - 1];
+		
+		// Check for the PC matrix
+		if (typeof(hdr.pc1_1) === 'undefined') {
+			this.pc = [[1, 0], [0, 1]];
+		} else {
+			this.pc = [
+				[parseFloat(hdr.pc1_1), parseFloat(hdr.pc1_2)],
+				[parseFloat(hdr.pc2_1), parseFloat(hdr.pc2_2)]
+			];	
+		}
+		this.cdelt = [deg_to_rad(parseFloat(hdr.cdelt1)), deg_to_rad(parseFloat(hdr.cdelt2))];
 		this.cunit = [hdr.cunit1, hdr.cunit2];
 		this.ctype = [hdr.ctype1, hdr.ctype2];
-		this.crval = [
-			parseFloat(hdr.crval1),
-			parseFloat(hdr.crval2)
-		];
-		this.lonpole = parseFloat(hdr.lonpole);
-		this.latpole = parseFloat(hdr.latpole);
+		this.crval = [deg_to_rad(parseFloat(hdr.crval1)), deg_to_rad(parseFloat(hdr.crval2))];
+		this.lonpole = deg_to_rad(parseFloat(hdr.lonpole));
+		this.latpole = deg_to_rad(parseFloat(hdr.latpole));
 		this.date_obs = hdr.date_obs;
 		
 		// Check the projection and define the appropriate function
 		if (this.ctype[0].slice(5) === 'TAN' && this.ctype[1].slice(5) === 'TAN') {
 
+			this.lat_0 = Math.PI / 2;
+
 			WCS.prototype.to_spherical = function (x, y) {
+				var r, theta, phi;
 
-				var phi, theta, r;
-
-				phi = Math.atan2(-y, x);
 				r = Math.sqrt(x*x + y*y);
-				
-				theta = Math.atan(180 / Math.PI * r);
-				
+				theta = this.lat_0 - Math.atan(r);
+				phi = Math.atan2(x, -y);
+
 				return [phi, theta];
-				
-			};
-		}
-		
-		var one;
-		one = false;
-		
-		if (one) {
-			WCS.prototype.blah = function() {
-				console.log("True");
-			};
-		} else {
-			WCS.prototype.blah = function() {
-				console.log("False");
 			};
 		}
 	};
@@ -71,34 +54,33 @@ function rad_to_deg(radian) {
 	WCS.prototype = {
 		
 		to_intermediate: function (points) {
-			var i, j, num_points, point, proj;
-			
-			num_points = points.length;
-			proj = [];
+			var i, proj;
 
-			for (i = 0; i < num_points; i += 1) {
-				point = 0;
-				for (j = 0; j < this.wcsaxes; j += 1) {
-					point += this.pc[i][j] * (points[j] - this.crpix[j]);
-				}
-				point = this.cdelt[i] * point;
-				proj.push(point);
+			for (i = 0; i < this.wcsaxes; i += 1) {
+				points[i] -= this.crpix[i];
 			}
+			proj = [];
+			proj.push(this.cdelt[0] * (this.pc[0][0] * points[0] + this.pc[0][1] * points[1]));
+			proj.push(this.cdelt[1] * (this.pc[1][0] * points[0] + this.pc[1][1] * points[1]));
 
 			return proj;
 		},
 
 		to_celestial: function (phi, theta) {
-			var alpha_p, delta_p, phi_p, alpha, delta;
-			
+			var alpha_p, delta_p, phi_p, alpha, delta, x, y;
+
 			alpha_p = this.crval[0];
 			delta_p = this.crval[1];
 			phi_p = this.lonpole;
+
+			x = -1 * Math.cos(theta) * Math.sin(phi - phi_p);
+			y = Math.sin(theta) * Math.cos(delta_p) - Math.cos(theta) * Math.sin(delta_p) * Math.cos(phi - phi_p);
 			
-			alpha = alpha_p
-			alpha += Math.atan2(Math.sin(deg_to_rad(theta)) * Math.sin(deg_to_rad(delta_p)) - Math.cos(deg_to_rad(theta)) * Math.sin(deg_to_rad(delta_p)) * Math.cos(deg_to_rad(phi - phi_p)), -Math.cos(deg_to_rad(theta)) * Math.sin(deg_to_rad(phi - phi_p)));
-			delta = Math.asin(Math.sin(deg_to_rad(theta)) * Math.sin(deg_to_rad(delta_p)) + Math.cos(deg_to_rad(theta)) * Math.cos(deg_to_rad(delta_p)) * Math.cos(deg_to_rad(phi - phi_p)));
-			
+			alpha = alpha_p + Math.atan2(x, y);
+			alpha += (2 * Math.PI);
+			alpha = alpha % (2 * Math.PI);
+			delta = Math.asin(Math.sin(theta) * Math.sin(delta_p) + Math.cos(theta) * Math.cos(delta_p) * Math.cos(phi - phi_p));
+
 			return [rad_to_deg(alpha), rad_to_deg(delta)];
 		},
 		
