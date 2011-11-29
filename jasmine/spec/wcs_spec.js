012f7f6ac3f585b3ebd2cf810a6ad1413cf3bc3c
Amit Kapadia <amit@zooniverse.org>
2011-11-29 15:37:40 -0600

    Fixed RA conversion.  Added multiple tests for each projection.

--- jasmine/spec/wcs_spec.js
+++ jasmine/spec/wcs_spec.js
@@ -142,122 +142,304 @@ describe ("pixel to sky transformations", function () {
 	//
 	
 	it ("ARC Projection", function() {
-		var wcs, pix, sky;
-		pix = [0, 0];
-		sky = [269.05673, -73.46830];
+		var wcs, pixels, sky, i;
+		pixels = [];
+		sky = [];
 		
+		pixels.push([0.0, 0.0]);
+		pixels.push([191.0, 0.0]);
+		pixels.push([0.0, 191.0]);
+		pixels.push([191.0, 191.0]);
+		pixels.push([95.5, 95.5]);
+
+		sky.push([269.05673077774, -73.46829958535]);
+		sky.push([269.46714963295, -60.73594102637]);
+		sky.push([307.01180433182, -69.29965938607]);
+		sky.push([293.06610193764, -58.19446383811]);
+		sky.push([284.90543739577, -66.30663097651]);
+
 		wcs = new WCS(arc);
-		expect(wcs.pix_to_sky(pix)).toBeCloseToElementwise(sky, 5);
+		for (i = 0; i < pixels.length; i += 1) {
+			expect(wcs.pix_to_sky(pixels[i])).toBeCloseToElementwise(sky[i], 5);
+		}
 	});
 
 	it ("SIN Projection", function () {
-		var wcs, pix, sky;
-		pix = [0, 0];
-		sky = [269.39151, -73.90354];
+		var wcs, pixels, sky, i;
+		pixels = [];
+		sky = [];
+
+		pixels.push([0.0, 0.0]);
+		pixels.push([191.0, 0.0]);
+		pixels.push([0.0, 191.0]);
+		pixels.push([191.0, 191.0]);
+		pixels.push([95.5, 95.5]);
+
+		sky.push([268.39150699215, -73.90353552624]);
+		sky.push([269.10716399624, -60.03668870909]);
+		sky.push([307.73275850866, -69.48636458818]);
+		sky.push([293.24065113325, -57.07877059966]);
+		sky.push([284.90376923726, -66.31039234200]);
 
 		wcs = new WCS(sin);
-		expect(wcs.pix_to_sky(pix)).toBeCloseToElementwise(sky, 5);
+		for (i = 0; i < pixels.length; i += 1) {
+			expect(wcs.pix_to_sky(pixels[i])).toBeCloseToElementwise(sky[i], 8);
+		}
 	});
 	
 	it ("STG Projection", function () {
-		var wcs, pix, sky;
-		pix = [0, 0];
-		sky = [269.37826, -73.25613];
+		var wcs, pixels, sky, i;
+		pixels = [];
+		sky = [];
+
+		pixels.push([0.0, 0.0]);
+		pixels.push([191.0, 0.0]);
+		pixels.push([0.0, 191.0]);
+		pixels.push([191.0, 191.0]);
+		pixels.push([95.5, 95.5]);
+		
+		sky.push([269.37825680266, -73.25613046025]);
+		sky.push([269.64574155082, -61.03602098451]);
+		sky.push([306.65846712587, -69.21034027654]);
+		sky.push([292.97934645515, -58.65820590407]);
+		sky.push([284.90625709548, -66.30490865995]);
 
 		wcs = new WCS(stg);
-		expect(wcs.pix_to_sky(pix)).toBeCloseToElementwise(sky, 5);
+		for (i = 0; i < pixels.length; i += 1) {
+			expect(wcs.pix_to_sky(pixels[i])).toBeCloseToElementwise(sky[i], 8);
+		}
 	});	
 	
 	it ("TAN Projection", function () {
-		var wcs, pix, sky;
-		pix = [0, 0];
-		sky = [270.33284, -72.61583];
+		var wcs, pixels, sky, i;
+		pixels = [];
+		sky = [];
+
+		pixels.push([0.0, 0.0]);
+		pixels.push([191.0, 0.0]);
+		pixels.push([0.0, 191.0]);
+		pixels.push([191.0, 191.0]);
+		pixels.push([95.5, 95.5]);
+		
+		sky.push([270.33283605009, -72.61583231845]);
+		sky.push([270.19465794261, -61.83923481247]);
+		sky.push([305.59026284675, -68.94388297928]);
+		sky.push([292.71201278074, -59.87298900275]);
+		sky.push([284.90874458094, -66.30003124798]);
 
 		wcs = new WCS(tan);
-		expect(wcs.pix_to_sky(pix)).toBeCloseToElementwise(sky, 5);
+		for (i = 0; i < pixels.length; i += 1) {
+			expect(wcs.pix_to_sky(pixels[i])).toBeCloseToElementwise(sky[i], 8);
+		}
 	});
 
 	it ("ZEA Projection", function () {
-		var wcs, pix, sky;
-		pix = [0, 0];
-		sky = [268.89430, -73.57490];
+		var wcs, pixels, sky, i;
+		pixels = [];
+		sky = [];
+
+		pixels.push([0.0, 0.0]);
+		pixels.push([191.0, 0.0]);
+		pixels.push([0.0, 191.0]);
+		pixels.push([191.0, 191.0]);
+		pixels.push([95.5, 95.5]);
+
+		sky.push([268.89429694488, -73.57489559933]);
+		sky.push([269.37808163249, -60.57684091173]);
+		sky.push([307.18909348552, -69.34483973313]);
+		sky.push([293.10932896827, -57.94570137254]);
+		sky.push([284.90502664962, -66.30752029696]);
 
 		wcs = new WCS(zea);
-		expect(wcs.pix_to_sky(pix)).toBeCloseToElementwise(sky, 5);
+		for (i = 0; i < pixels.length; i += 1) {
+			expect(wcs.pix_to_sky(pixels[i])).toBeCloseToElementwise(sky[i], 8);
+		}
 	});
 
 	//
 	// Cylindrical Projections
 	//
 	it ("CYP Projection", function () {
-		var wcs, pix, sky;
-		pix = [0, 0];
-		sky = [263.69301, -75.95480];
+		var wcs, pixels, sky, i;
+		pixels = [];
+		sky = [];
+
+		pixels.push([0.0, 0.0]);
+		pixels.push([191.0, 0.0]);
+		pixels.push([0.0, 191.0]);
+		pixels.push([191.0, 191.0]);
+		pixels.push([95.5, 95.5]);
+		
+		sky.push([263.69300640788, -75.95480262512]);
+		sky.push([267.11650280878, -57.99620775699]);
+		sky.push([314.47700550214, -70.78754655132]);
+		sky.push([294.10767800687, -55.63518650347]);
+		sky.push([284.89605048654, -66.32061940296]);
 		
 		wcs = new WCS(cyp);
-		expect(wcs.pix_to_sky(pix)).toBeCloseToElementwise(sky, 5);
+		for (i = 0; i < pixels.length; i += 1) {
+			expect(wcs.pix_to_sky(pixels[i])).toBeCloseToElementwise(sky[i], 8);
+		}
 	});
 
 	it ("CEA Projection", function () {
-		var wcs, pix, sky;
-		pix = [0, 0];
-		sky = [268.44085, -73.37969];
+		var wcs, pixels, sky, i;
+		pixels = [];
+		sky = [];
+
+		pixels.push([0.0, 0.0]);
+		pixels.push([191.0, 0.0]);
+		pixels.push([0.0, 191.0]);
+		pixels.push([191.0, 191.0]);
+		pixels.push([95.5, 95.5]);
+		
+		sky.push([268.44085265462, -73.37969380549]);
+		sky.push([269.09024385942, -60.64908874812]);
+		sky.push([307.52044819239, -69.38302901111]);
+		sky.push([294.13191054912, -58.36209566279]);
+		sky.push([284.90109941802, -66.30599022330]);
 		
 		wcs = new WCS(cea);
-		expect(wcs.pix_to_sky(pix)).toBeCloseToElementwise(sky, 5);
+		for (i = 0; i < pixels.length; i += 1) {
+			expect(wcs.pix_to_sky(pixels[i])).toBeCloseToElementwise(sky[i], 8);
+		}
 	});
 
 	it ("CAR Projection", function () {
-		var wcs, pix, sky;
-		pix = [0, 0];
-		sky = [268.47851, -73.37997];
+		var wcs, pixels, sky, i;
+		pixels = [];
+		sky = [];
+
+		pixels.push([0.0, 0.0]);
+		pixels.push([191.0, 0.0]);
+		pixels.push([0.0, 191.0]);
+		pixels.push([191.0, 191.0]);
+		pixels.push([95.5, 95.5]);
+		
+		sky.push([268.47850587888, -73.37997130772]);
+		sky.push([269.11222126114, -60.64923604906]);
+		sky.push([307.32299968118, -69.43277061051]);
+		sky.push([293.97962362308, -58.39244690857]);
+		sky.push([284.90153565747, -66.30594750654]);
 		
 		wcs = new WCS(car);
-		expect(wcs.pix_to_sky(pix)).toBeCloseToElementwise(sky, 5);
+		for (i = 0; i < pixels.length; i += 1) {
+			expect(wcs.pix_to_sky(pixels[i])).toBeCloseToElementwise(sky[i], 8);
+		}
 	});
 
 	it ("MER Projection", function () {
-		var wcs, pix, sky;
-		pix = [0, 0];
-		sky = [268.51628, -73.38024];
+		var wcs, pixels, sky, i;
+		pixels = [];
+		sky = [];
+
+		pixels.push([0.0, 0.0]);
+		pixels.push([191.0, 0.0]);
+		pixels.push([0.0, 191.0]);
+		pixels.push([191.0, 191.0]);
+		pixels.push([95.5, 95.5]);
+		
+		sky.push([268.51628090050, -73.38024288395]);
+		sky.push([269.13426924501, -60.64938020277]);
+		sky.push([307.13073648219, -69.48077163725]);
+		sky.push([293.83175889043, -58.42169429877]);
+		sky.push([284.90196957302, -66.30590501598]);
 		
 		wcs = new WCS(mer);
-		expect(wcs.pix_to_sky(pix)).toBeCloseToElementwise(sky, 5);
+		for (i = 0; i < pixels.length; i += 1) {
+			expect(wcs.pix_to_sky(pixels[i])).toBeCloseToElementwise(sky[i], 8);
+		}
 	});
 
 	it ("SFL Projection", function () {
-		var wcs, pix, sky;
-		pix = [0, 0];
-		sky = [268.46738, -73.50406];
+		var wcs, pixels, sky, i;
+		pixels = [];
+		sky = [];
+
+		pixels.push([0.0, 0.0]);
+		pixels.push([191.0, 0.0]);
+		pixels.push([0.0, 191.0]);
+		pixels.push([191.0, 191.0]);
+		pixels.push([95.5, 95.5]);
+		
+		sky.push([268.46737987111, -73.50405652146]);
+		sky.push([269.10879603428, -60.77298321216]);
+		sky.push([306.90291088836, -69.22328916258]);
+		sky.push([293.61495994868, -57.87845261548]);
+		sky.push([284.90245830848, -66.30746896901]);
 		
 		wcs = new WCS(sfl);
-		expect(wcs.pix_to_sky(pix)).toBeCloseToElementwise(sky, 5);
+		for (i = 0; i < pixels.length; i += 1) {
+			expect(wcs.pix_to_sky(pixels[i])).toBeCloseToElementwise(sky[i], 8);
+		}
 	});
 
 	it ("PAR Projection", function () {
-		var wcs, pix, sky;
-		pix = [0, 0];
-		sky = [269.47944, -73.49563];
+		var wcs, pixels, sky, i;
+		pixels = [];
+		sky = [];
+
+		pixels.push([0.0, 0.0]);
+		pixels.push([191.0, 0.0]);
+		pixels.push([0.0, 191.0]);
+		pixels.push([191.0, 191.0]);
+		pixels.push([95.5, 95.5]);
+		
+		sky.push([269.47944138196, -73.49563038873]);
+		sky.push([269.69722708408, -60.76256251520]);
+		sky.push([306.32989302475, -69.41721246570]);
+		sky.push([293.18212343779, -58.05648713597]);
+		sky.push([284.90591012398, -66.30689603965]);
 		
 		wcs = new WCS(par);
-		expect(wcs.pix_to_sky(pix)).toBeCloseToElementwise(sky, 5);
+		for (i = 0; i < pixels.length; i += 1) {
+			expect(wcs.pix_to_sky(pixels[i])).toBeCloseToElementwise(sky[i], 8);
+		}
 	});
 
 	it ("MOL Projection", function () {
-		var wcs, pix, sky;
-		pix = [0, 0];
-		sky = [270.72846, -74.16980];
+		var wcs, pixels, sky, i;
+		pixels = [];
+		sky = [];
+
+		pixels.push([0.0, 0.0]);
+		pixels.push([191.0, 0.0]);
+		pixels.push([0.0, 191.0]);
+		pixels.push([191.0, 191.0]);
+		pixels.push([95.5, 95.5]);
+		
+		sky.push([270.72846180802, -74.16980073050]);
+		sky.push([270.39774981058, -60.02718873940]);
+		sky.push([306.84442527764, -70.24462868694]);
+		sky.push([292.26796386222, -57.66494958439]);
+		sky.push([284.91211049555, -66.30961122414]);
 		
 		wcs = new WCS(mol);
-		expect(wcs.pix_to_sky(pix)).toBeCloseToElementwise(sky, 5);
+		for (i = 0; i < pixels.length; i += 1) {
+			expect(wcs.pix_to_sky(pixels[i])).toBeCloseToElementwise(sky[i], 8);
+		}
 	});
 
 	it ("AIT Projection", function () {
-		var wcs, pix, sky;
-		pix = [0, 0];
-		sky = [268.56814, -73.49846];
+		var wcs, pixels, sky, i;
+		pixels = [];
+		sky = [];
+
+		pixels.push([0.0, 0.0]);
+		pixels.push([191.0, 0.0]);
+		pixels.push([0.0, 191.0]);
+		pixels.push([191.0, 191.0]);
+		pixels.push([95.5, 95.5]);
+		
+		sky.push([268.56813922636, -73.49845984257]);
+		sky.push([269.17359044102, -60.70174516331]);
+		sky.push([307.08620023155, -69.28342195718]);
+		sky.push([293.58502491896, -57.98593060648]);
+		sky.push([284.90284110443, -66.30720454723]);
 
 		wcs = new WCS(ait);
-		expect(wcs.pix_to_sky(pix)).toBeCloseToElementwise(sky, 5);
+		for (i = 0; i < pixels.length; i += 1) {
+			expect(wcs.pix_to_sky(pixels[i])).toBeCloseToElementwise(sky[i], 8);
+		}
 	});
 });
