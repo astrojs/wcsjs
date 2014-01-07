d9ad6559b7db7cc24b3cd0f25e89f4ef1fcc685c
Amit Kapadia <akapad@gmail.com>
2014-01-07 13:09:06 -0800

    more uniform image processing

--- images/make_data.py
+++ images/make_data.py
@@ -7,16 +7,26 @@ import Image
 
 
 def make_images():
+    
+    # Get the global min and max pixel
+    extents = []
+    for f in glob.glob(path.join('..', 'test', 'data',"*.fits.gz")):
+        data = fits.getdata(f)
+        extents.append( np.nanmin(data) )
+        extents.append( np.nanmax(data) )
+    
+    extents = np.array(extents)
+    minimum = extents.min()
+    maximum = extents.max()
+    extent = float(maximum - minimum)
+    extent = extent - 0.75 * extent
+    
     for f in glob.glob(path.join('..', 'test', 'data', "*.fits.gz")):
-        
         data = fits.getdata(f)
         
-        minimum = np.nanmin(data)
-        extent = float(np.nanmax(data) - minimum)
-        extent = extent - 0.5 * extent
-        
         data = (data - minimum) / extent
-        data = (255.0 * data).astype('uint8')
+        data = (255.0 * data)
+        data = np.clip(data, 0, 255).astype('uint8')
         data = np.flipud(data)
         
         im = Image.fromarray(data)
