5674ba036f5ca821ec8bfae1605c4feb75605ffd
Amit Kapadia <akapad@gmail.com>
2014-01-06 18:27:58 -0800

    flipped image to align with coordinates

--- images/make_data.py
+++ images/make_data.py
@@ -1,4 +1,5 @@
 import glob
+from os import path
 import json
 import numpy as np
 from astropy.io import fits
@@ -6,7 +7,7 @@ import Image
 
 
 def make_images():
-    for f in glob.glob("*.fits.gz"):
+    for f in glob.glob(path.join('..', 'test', 'data', "*.fits.gz")):
         
         data = fits.getdata(f)
         
@@ -16,13 +17,14 @@ def make_images():
         
         data = (data - minimum) / extent
         data = (255.0 * data).astype('uint8')
+        data = np.flipud(data)
         
         im = Image.fromarray(data)
         im.save(f.replace('fits.gz', 'png'))
 
 def make_headers():
     headers = {}
-    for f in glob.glob("*.fits.gz"):
+    for f in glob.glob(path.join('..', 'test', 'data', "*.fits.gz")):
         header = fits.getheader(f)
         
         del header["HISTORY"]
@@ -33,4 +35,4 @@ def make_headers():
 
 
 if __name__ == '__main__':
-    make_headers()
+    make_images()
